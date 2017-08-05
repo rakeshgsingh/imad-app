4ce247fdf6436493ab2af865abc9bf94b73b2050
Rakesh Singh <rakeshgsingh@gmail.com>
2017-08-05 18:29:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ var htmlTemplate = `
         <html>
             <head>
                 <title> ${title}</title>
-                <meta name="viewport" content ="width=width-device, initial-scale=1"/>
+                <meta name="viewport" content ="width=device-width, initial-scale=1"/>
                  <link href="/ui/style.css" rel="stylesheet" />
             </head>
             <body>
