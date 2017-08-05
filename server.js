1e3281c316a9abd650a6726d140b20baf3ba3e07
Rakesh Singh <rakeshgsingh@gmail.com>
2017-08-05 09:31:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article-one', function(req,res){
     res.send('Article one is requested and will be serveed here');
 });
 
+app.get('/article-one', function(req,res){
+    res.send('Article one is requested and will be serveed here');
+});
+
+app.get('/article-one', function(req,res){
+    res.send('Article one is requested and will be serveed here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
