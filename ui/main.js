07538a31a692a9b75c3bb01468cb3677250ed919
Rakesh Singh <rakeshgsingh@gmail.com>
2017-08-05 20:40:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 
 var element = document.getElementById("main-text");
 element.innerHTML = 'New value';
+
+// move the image
+var img = document.getElementById('madi');
+img.onClick=function(){
+    img.style.marginLeft ='100px';
+};
