4cb386db761c33dda335b68226596f041aa57688
Rakesh Singh <rakeshgsingh@gmail.com>
2017-08-07 21:02:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,21 @@ button.onclick = function(){
         request.open('GET','http://rakeshgsingh.imad.hasura-app.io/counter', true);
         request.send(null);
  };
+ // submit name
+ var nameInput = document.getElementByid("name");
+ var name = nameInput.value;
+ var submit = document.getElementByid("submit_btn");
+ submit.onclick = function(){
+     // make a request to server and send the name.
+     
+     //capture a list of name and render it as a list.
+     var names = ['name1','name2','name3'];
+     var list = '';
+     for(var i=0; i<names.length; i++){
+         list == '<li>' + [name(i)] + '</li>';
+         var ul = document.getElementById("namelist")
+         ul.innerHTML = list;
+         
+     }
+     
+ };
