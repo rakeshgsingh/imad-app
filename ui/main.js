fbe926faa73df9f402e36fdea934d7bc5b00541e
Rakesh Singh <rakeshgsingh@gmail.com>
2017-08-08 19:31:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,17 @@
 //counter code
 var button = document.getElementById("counter");
-
 button.onclick = function(){
-    
    //Creat a request object
     var request = new XMLHttpRequest();
-    
     //capture the response and stored in a variable
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.Done){
-            
             //take some action
-            
             if(request.status === 200){
               var counter =  request.responseText;
              var span = document.getElementById("count");
              span.innerHTML = counter.toString(); 
             }
-            
         }
         //Not done yet
     };
@@ -27,21 +21,15 @@ button.onclick = function(){
  };
  
  // submit name
-
  var submit = document.getElementById("submit_btn");
- 
  submit.onclick = function(){
-     
-     //Creat a request object
+    //Creat a request object
     var request = new XMLHttpRequest();
-    
     //capture the response and stored in a variable
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.Done){
-            
             //take some action
-            
-            if(request.status === 200){
+             if(request.status === 200){
                 //capture a list of name and render it as a list.
              var names = request.responseText;
              names = JSON.parse(names);
@@ -50,19 +38,14 @@ button.onclick = function(){
                  list += '<li>' + names[i] + '</li>';
              }
                  document.getElementById("namelist").innerHTML = list;
-                
-            
-            
         }
     }          
-     
-        //not done yet 
-     };
+             //not done yet 
+  };
         //make a request
          var nameInput = document.getElementById("name");
          var name = nameInput.value;
- 
-        request.open('GET','http://rakeshgsingh.imad.hasura-app.io/submit-name?name=' + name, true);
+         request.open('GET','http://rakeshgsingh.imad.hasura-app.io/submit-name?name=' + name, true);
         request.send(null);
  };     
  
