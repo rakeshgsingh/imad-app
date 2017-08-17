77bfb4a0a003a0260dbd33b2c178ef4ace450267
Rakesh Singh <rakeshgsingh@gmail.com>
2017-08-17 07:28:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
+//var articles = {
    'article-one':{
         title :'Article-one | Rakesh Singh',
         heading :'Article-one',
