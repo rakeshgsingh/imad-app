08837de1ab48a0d55a5541522e3cf8b54f2e2914
Rakesh Singh <rakeshgsingh@gmail.com>
2017-08-23 20:20:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,6 +83,33 @@ app.post('/create-user', function(req, res){
         
     });
 });
+
+app.post('/login', function(req, res){
+    //username,password
+    //{"username":"rakesh", "password":"password"}
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else{
+            if(result.rows.length === 0){
+                res.send(403).send('username/password is invalid');
+            }else {
+                //Match the password
+                var dbString = result.rows[0].password;
+                var salt = dbString.split('$')[2];
+                var hashedPassword = hash(password,salt);// creating a hash basedon the password submited and the original salt
+                if(hashPassword ===dbString){
+                res.send('Credintials are correct');
+                }else {
+                    res.send(403).send('username/password is invalid');
+                }
+            }
+        }      
+      });
+        
+    });
 // Connect the database for Table test
 var pool = new Pool(config);
 app.get('/test-db', function(req, res){
