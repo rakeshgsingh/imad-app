e1474b8127b6d5a9ad9827eee3fb16a27e53d90f
Rakesh Singh <rakeshgsingh@gmail.com>
2017-08-26 19:14:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -228,7 +228,7 @@ app.get('/articles/:articleName', function (req, res) {
     }
   });
 });
-
+/*
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
@@ -236,7 +236,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+*/
 app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
