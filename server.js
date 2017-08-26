3e985849ff176b4fd6b57e8eceb225b12b3d603e
Rakesh Singh <rakeshgsingh@gmail.com>
2017-08-26 19:04:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
-    resave: true,
-    saveUninitialized: true,
+    //resave: true,
+    //saveUninitialized: true,
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
 
